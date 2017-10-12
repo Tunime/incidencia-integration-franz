83a5fe7ca438a602efbfead0d550d253cb7a68da
AlvaroMoCa <almaseb_123@hotmail.com>
2017-10-12 00:41:23 -0500

    incoporando los modales

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+function modalvisto() {
+    document.getElementById("modal-datos").style.top="0"
+}
+function modalcerrar() {
+    document.getElementById("modal-datos").style.top="-100vh"
+}
+function modalvistoeditar() {
+    document.getElementById("modal-datos-editar").style.top="0"
+}
+function modalcerrareditar() {
+    document.getElementById("modal-datos-editar").style.top="-100vh"
+}
