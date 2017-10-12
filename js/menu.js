bbd9e72c628af2cc51cf23df8128397f70eda3d2
AlvaroMoCa <almaseb_123@hotmail.com>
2017-10-12 00:18:51 -0500

    Incorporando el proyecto en este repositorio

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btnmodal = document.getElementById('openmenu');
+var menubg = document.getElementById('menuopt');
+
+btnmodal.addEventListener('click',function(){
+    menubg.classList.toggle('lookmenu');
+})
